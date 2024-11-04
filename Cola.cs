5396fa4bf1968c23935d86e3d54e248fa76df674
cisco01 <eduarosantoyo@gmail.com>
2024-11-04 13:22:32 -0700

    Agregar archivos de proyecto.

---
+++ Cola.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Cola_EESA
+{
+    internal class Cola
+    {
+        private NODO inicio;
+        private int count = 0;
+        private int MAX;
+        public Cola(int max)
+        {
+            MAX = max;
+            inicio = null;
+        }
+        private bool UnderFlow()
+        {
+            if (inicio == null)
+                return true;
+            else
+                return false;
+        }
+        private bool OverFlow()
+        {
+            if (count == MAX)
+                return true;
+            else
+                return false;
+        }
+        public void Print()
+        {
+
+        }
+        public int Count()
+        {
+            return count;
+        }
+        public bool Insert()
+        {
+            //Regresa true al insertar y si no inserta regresa false por estar llena o otro pedo
+            return false;
+        }
+        public int Extract()
+        {
+            //Regresa valor extraido, si está vacia o no pudo extraer regresa -1
+            return -1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Proyecto_Cola_EESA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random R = new Random(25);
+            Console.WriteLine(R);
+        }
+    }
+}
