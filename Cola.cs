1307ebb519bd841fdddde472e6e97ca2d75c9b93
tncsito <eduarosantoyo@gmail.com>
2024-11-05 13:33:22 -0700

    parte logica termina

--- Cola.cs
+++ Cola.cs
@@ -16,7 +16,7 @@ namespace Proyecto_Cola_EESA
             MAX = max;
             inicio = null;
         }
-        private bool UnderFlow()
+        private bool UnderFlow() //Si es true está libre 
         {
             if (inicio == null)
                 return true;
@@ -30,21 +30,70 @@ namespace Proyecto_Cola_EESA
             else
                 return false;
         }
+        public void Size(int n)
+        {
+             MAX = n;
+        }
         public void Print()
         {
-
+            if (inicio == null)
+            {
+                Console.WriteLine("La pila está vacía");
+            }
+            else
+            {
+                Console.Write("Base => ");
+                NODO act = inicio;
+                while (act != null)
+                {
+                    Console.Write($"{act.Valor} => ");
+                    act = act.Sig;
+                }
+                Console.WriteLine("Inicio");
+            }
         }
         public int Count()
         {
             return count;
         }
-        public bool Insert()
+        public bool Insert(int num)
         {
+            NODO nuevo = new NODO(num);
+            if (!OverFlow())
+            {
+                if (UnderFlow() == true)
+                {
+                    inicio = nuevo;
+                    count++;
+                    return true;
+                }
+                else
+                {
+                    NODO act = inicio;
+                    while (act.Sig != null)
+                    {
+                        act = act.Sig;
+                    }
+                    act.Sig = nuevo;
+                    count++;
+                    return true;
+                }
+            }//si está llena la cola marca false
             //Regresa true al insertar y si no inserta regresa false por estar llena o otro pedo
             return false;
         }
         public int Extract()
         {
+            if (!UnderFlow())
+            {
+                int Valor;
+                NODO act;
+                act = inicio;
+                Valor = Convert.ToInt32(inicio);
+                inicio = act.Sig;
+                count--;
+                return Valor;
+            }
             //Regresa valor extraido, si está vacia o no pudo extraer regresa -1
             return -1;
         }
--- Program.cs
+++ Program.cs
@@ -4,8 +4,65 @@
     {
         static void Main(string[] args)
         {
-            Random R = new Random(25);
-            Console.WriteLine(R);
+            bool p = true;
+            int n = 99;
+            Cola a = new Cola(n); // Inicializa la pila fuera del bucle
+
+            while (p)
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("\tMenu");
+                    Console.WriteLine("-------------------------");
+                    Console.WriteLine("1.- Set Size");              //Prueba
+                    Console.WriteLine("2.- Insert");                //Prueba
+                    Console.WriteLine("3.- Extract");               //Prueba
+                    Console.WriteLine("4.- Print");                 //Prueba
+                    Console.WriteLine("5.- Exit");                  //Prueba
+                    Console.WriteLine("-------------------------");
+                    Console.Write("Select Option => ");
+                    int opc = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    switch (opc)
+                    {
+                        case 1: //Set Size
+                            Console.Write("Escribe el nuevo tamaño de la pila => ");
+                            n = Convert.ToInt32(Console.ReadLine());
+                            a.Size(n);
+                            Console.Clear();
+                            Console.WriteLine("¡Tamaño de Cola ajustado correctamente! \n");
+                            break;
+                        case 2: //Insert
+
+                            break;
+                        case 3: //Extract
+
+                            break;
+                        case 4: //Print
+
+                            break;
+                        case 5: //Salir
+                            p = false;
+                            return;
+                        default:
+                            Console.WriteLine("\nPor favor Seleccione una Opción");
+                            break;
+
+                    }
+                    Console.Write("\n\n\n\n\n\n\t<Presiona cualquier tecla para continuar>");
+                    Console.ReadKey();
+                }
+                catch (Exception e)
+                {
+                    Console.WindowHeight = 10;
+                    Console.WindowWidth = 60;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Clear();
+                    Console.WriteLine("\n\n\n\n\tCaracter no válido <ENTER PARA CONTINUAR>");
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
